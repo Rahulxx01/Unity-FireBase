77f3076b5ad8ceee240009e862ae018ff1f393c7
RAHUL YADAV <ryadav531996@gmail.com>
2018-03-28 18:33:21 +0530

    First Commit
    First Commit

---
+++ Assets/script/FirebaseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+public class FirebaseScript : MonoBehaviour {
+	public InputField Emailid, Password;
+
+	public void LoginButtonPressed(){
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync (Emailid.text, Password.text).
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("New Scene");
+		});
+	}
+/*
+	public void LoginButtonPressed(){
+		FirebaseAuth.DefaultInstance.SignInWithCredentialAsync (Emailid.text, Password.text).
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("New Scene");
+		});
+	}*/
+	public void SignupButtonPressed(){
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync (Emailid.text, Password.text).
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("New Scene");
+		});
+	}
+
+
+	}
---
+++ Assets/script/Register.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour {
+	public GameObject firstname;
+	public GameObject lastname;
+	public GameObject username;
+	public GameObject email;
+	public GameObject password;
+	public GameObject confPasword;
+
+	private string Firstname;
+	private string Lastname;
+	private string Username;
+	private string Email;
+	private string Password;
+	private string ConfPassword;
+	private string form;
+	private bool EmailValid = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Firstname = firstname.GetComponent<InputField> ().text;
+		Lastname = lastname.GetComponent<InputField> ().text;
+		Username = username.GetComponent<InputField> ().text;
+		Email = email.GetComponent<InputField> ().text;
+		Password = password.GetComponent<InputField> ().text;
+		//Confpassword = confPassword.GetComponent<InputField> ().text;
+	}
+}
